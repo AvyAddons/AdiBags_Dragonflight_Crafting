9f00c8e57c4f30522e6f63a9c07d332d78735884
Lucas Vienna <dev@lucasvienna.dev>
2022-10-29 22:55:49 +0200

    Add options panel

--- AdiBags_DFCrafting/Main.lua
+++ AdiBags_DFCrafting/Main.lua
@@ -117,6 +117,10 @@ function filter:Filter(slotData)
 	if (itemId and CacheIds[itemId]) then
 		return CacheIds[itemId]
 	end
+
+	-- TODO: some addons have a tooltip here, for whatever reason
+	-- even if they're tracking by ID and not by tt scanning. 
+	-- figure out why
 end
 
 function filter:StartCache()
@@ -207,6 +211,137 @@ end
 function filter:GetOptions()
 	return {
 		-- Setup for the options panel
+		move_alchemy = {
+			name = L[self.db.profile.alchemy.name],
+			desc = L[self.db.profile.alchemy.desc],
+			type = "toggle",
+			order = 0,
+		},
+		move_cloth = {
+			name = L[self.db.profile.cloth.name],
+			desc = L[self.db.profile.cloth.desc],
+			type = "toggle",
+			order = 2,
+		},
+		move_cooking = {
+			name = L[self.db.profile.cooking.name],
+			desc = L[self.db.profile.cooking.desc],
+			type = "toggle",
+			width = "double",
+			order = 3,
+		},
+		cooking_splits = {
+			name = L[self.db.profile.cooking.name],
+			desc = L[self.db.profile.cooking.desc], -- doesn't seem to get used anyway
+			type = "group",
+			inline = true,
+			order = 4,
+			disabled = function () return not self.db.profile.move_cooking end,
+			args = {
+				split_tuskarr_feast = {
+					name = L[self.db.profile.cooking.tuskarr_feast.name],
+					desc = L[self.db.profile.cooking.tuskarr_feast.desc],
+					type = "toggle",
+					order = 10,
+				},
+				split_ingredients = {
+					name = L[self.db.profile.cooking.ingredients.name],
+					desc = L[self.db.profile.cooking.ingredients.desc],
+					type = "toggle",
+					order = 20,
+				},
+				split_meat = {
+					name = L[self.db.profile.cooking.meat.name],
+					desc = L[self.db.profile.cooking.meat.desc],
+					type = "toggle",
+					order = 30,
+				},
+				split_fish = {
+					name = L[self.db.profile.cooking.fish.name],
+					desc = L[self.db.profile.cooking.fish.desc],
+					type = "toggle",
+					order = 40,
+				},
+				split_reagents = {
+					name = L[self.db.profile.cooking.reagents.name],
+					desc = L[self.db.profile.cooking.reagents.desc],
+					type = "toggle",
+					order = 50,
+				},
+			}
+		},
+		move_enchanting = {
+			name = L[self.db.profile.enchanting.name],
+			desc = L[self.db.profile.enchanting.desc],
+			type = "toggle",
+			order = 6,
+		},
+		move_herbs = {
+			name = L[self.db.profile.herbs.name],
+			desc = L[self.db.profile.herbs.desc],
+			type = "toggle",
+			order = 8,
+		},
+		move_inscription = {
+			name = L[self.db.profile.inscription.name],
+			desc = L[self.db.profile.inscription.desc],
+			type = "toggle",
+			order = 10,
+		},
+		move_jewelcrafting = {
+			name = L[self.db.profile.jewelcrafting.name],
+			desc = L[self.db.profile.jewelcrafting.desc],
+			type = "toggle",
+			order = 12,
+		},
+		move_leather = {
+			name = L[self.db.profile.leather.name],
+			desc = L[self.db.profile.leather.desc],
+			type = "toggle",
+			order = 14,
+		},
+		move_ore_stone = {
+			name = L[self.db.profile.ore_stone.name],
+			desc = L[self.db.profile.ore_stone.desc],
+			type = "toggle",
+			order = 16,
+		},
+		move_parts = {
+			name = L[self.db.profile.parts.name],
+			desc = L[self.db.profile.parts.desc],
+			type = "toggle",
+			order = 18,
+		},
+		move_darkmoon_cards = {
+			name = L[self.db.profile.darkmoon_cards.name],
+			desc = L[self.db.profile.darkmoon_cards.desc],
+			type = "toggle",
+			order = 20,
+		},
+		move_reagents = {
+			name = L[self.db.profile.reagents.name],
+			desc = L[self.db.profile.reagents.desc],
+			type = "toggle",
+			order = 22,
+		},
+		move_crafting = {
+			name = L[self.db.profile.crafting.name],
+			desc = L[self.db.profile.crafting.desc],
+			type = "toggle",
+			order = 24,
+		},
+		move_treasure_sack = {
+			name = L[self.db.profile.treasure_sack.name],
+			desc = L[self.db.profile.treasure_sack.desc],
+			type = "toggle",
+			order = 26,
+		},
+		move_fortune_card = {
+			name = L[self.db.profile.fortune_card.name],
+			desc = L[self.db.profile.fortune_card.desc],
+			type = "toggle",
+			order = 28,
+		},
 	}, AdiBags:GetOptionHandler(self, true, function() return self:Update() end)
 end
 
