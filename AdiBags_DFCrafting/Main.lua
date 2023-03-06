66bca261633c611dd5b0f0dcd2e8f3d011628449
Lucas Vienna <dev@lucasvienna.dev>
2023-03-06 13:22:01 +0100

    Dynamically fetch option and cache keys

--- AdiBags_DFCrafting/Main.lua
+++ AdiBags_DFCrafting/Main.lua
@@ -65,29 +65,20 @@ filter.uiName = L["Dragonflight Crafting"]
 filter.uiDesc = L["Put Dragonflight Crafting items and Reagents into their own categories."]
 
 function filter:OnInitialize()
+	-- Fetch our category option names dynamically
+	local profile = {}
+	for key, value in pairs(DB) do
+		profile[key] = true
+		-- handle categories with sections
+		if not value.items then
+			for k, v in pairs(value) do
+				-- skip the text only values
+				if (type(v) == 'table') then profile[k] = false end
+			end
+		end
+	end
 	-- Register the settings namespace
-	self.db = AdiBags.db:RegisterNamespace(self.filterName, {
-		profile = {
-			-- Add your settings here
-			move_alchemy = true,
-			move_cloth = true,
-			move_cooking = true,
-			split_tuskarr_feast = false,
-			split_ingredients = false,
-			split_meat = false,
-			split_fish = false,
-			split_reagents = false,
-			move_enchanting = true,
-			move_herbs = true,
-			move_inscription = true,
-			move_jewelcrafting = true,
-			move_leather = true,
-			move_ore_stone = true,
-			move_parts = true,
-			move_reagents = true,
-			move_crafting = true,
-		},
-	})
+	self.db = AdiBags.db:RegisterNamespace(self.filterName, { profile = profile })
 	-- Pre-populate our cache now that we have the settings loaded
 	Cache = self:BuildCache()
 end
@@ -123,191 +114,84 @@ end
 function filter:BuildCache()
 	local ids = {}
 
-	if self.db.profile.move_alchemy then
-		enableIds(ids, DB.alchemy)
-	end
-	if self.db.profile.move_cloth then
-		enableIds(ids, DB.cloth)
-	end
-	if self.db.profile.move_cooking then
-		-- iterate all cooking splits
-		for key, value in pairs(DB.cooking) do
-			-- skip the text only values
-			if (type(value) == 'table') then
-				if (self.db.profile["split_" .. key] == true) then
-					enableIds(ids, value)
-				else
-					-- override the section, only pass the cooking category
-					local section = value.section
-					value.section = nil
-					enableIds(ids, value)
-					-- if we don't restore this value, subsequent activations will
-					-- return the nil
-					value.section = section
+	for key, value in pairs(DB) do
+		if (self.db.profile[key] and value.items) then
+			-- Register the regular depth-1 categories
+			enableIds(ids, value)
+		elseif (self.db.profile[key] and not value.tems) then
+			-- Handle categories with sections instead of a single, top-level
+			-- `items` table.
+			for k, v in pairs(value) do
+				-- Skip the text only values, we only want the child tables
+				if (type(v) == 'table') then
+					if (self.db.profile[k] == true) then
+						enableIds(ids, v)
+					else
+						-- override the section, only pass the cooking category
+						local section = v.section
+						v.section = nil
+						enableIds(ids, v)
+						-- if we don't restore this value, subsequent activations will
+						-- return the nil
+						v.section = section
+					end
 				end
 			end
 		end
 	end
-	if self.db.profile.move_enchanting then
-		enableIds(ids, DB.enchanting)
-	end
-	if self.db.profile.move_herbs then
-		enableIds(ids, DB.herbs)
-	end
-	if self.db.profile.move_inscription then
-		enableIds(ids, DB.inscription)
-	end
-	if self.db.profile.move_jewelcrafting then
-		enableIds(ids, DB.jewelcrafting)
-	end
-	if self.db.profile.move_leather then
-		enableIds(ids, DB.leather)
-	end
-	if self.db.profile.move_ore_stone then
-		enableIds(ids, DB.ore_stone)
-	end
-	if self.db.profile.move_parts then
-		enableIds(ids, DB.parts)
-	end
-	if self.db.profile.move_reagents then
-		enableIds(ids, DB.reagents)
-	end
-	if self.db.profile.move_crafting then
-		enableIds(ids, DB.crafting)
-	end
 
-	addon.CacheIds = ids
 	return ids
 end
 
 -- Filter Options Panel
 -----------------------------------------------------------
 function filter:GetOptions()
-	return {
-		-- Setup for the options panel
+	local options = {
+		-- Setup for the options panel headers
 		craft = {
 			name = L["Crafting"],
 			type = "header",
 			order = 5,
 		},
-		move_alchemy = {
-			name = L[DB.alchemy.name],
-			desc = L[DB.alchemy.desc],
-			type = "toggle",
-			order = 10,
-		},
-		move_enchanting = {
-			name = L[DB.enchanting.name],
-			desc = L[DB.enchanting.desc],
-			type = "toggle",
-			order = 11,
-		},
-		move_inscription = {
-			name = L[DB.inscription.name],
-			desc = L[DB.inscription.desc],
-			type = "toggle",
-			order = 12,
-		},
-		move_jewelcrafting = {
-			name = L[DB.jewelcrafting.name],
-			desc = L[DB.jewelcrafting.desc],
-			type = "toggle",
-			order = 13,
-		},
-		move_parts = {
-			name = L[DB.parts.name],
-			desc = L[DB.parts.desc],
-			type = "toggle",
-			order = 14,
-		},
-		move_crafting = {
-			name = L[DB.crafting.name],
-			desc = L[DB.crafting.desc],
-			type = "toggle",
-			order = 15,
-		},
-		move_reagents = {
-			name = L[DB.reagents.name],
-			desc = L[DB.reagents.desc],
-			type = "toggle",
-			order = 16,
-		},
-		move_cooking = {
-			name = L[DB.cooking.name],
-			desc = L[DB.cooking.desc],
-			type = "toggle",
-			width = "double",
-			order = 17,
-		},
-		cooking_splits = {
-			name = L["Split Cooking"],
-			desc = "", -- doesn't seem to get used anyway
-			type = "group",
-			inline = true,
-			order = 18,
-			disabled = function() return not self.db.profile.move_cooking end,
-			args = {
-				split_tuskarr_feast = {
-					name = L[DB.cooking.tuskarr_feast.name],
-					desc = L[DB.cooking.tuskarr_feast.desc],
-					type = "toggle",
-					order = 10,
-				},
-				split_ingredients = {
-					name = L[DB.cooking.ingredients.name],
-					desc = L[DB.cooking.ingredients.desc],
-					type = "toggle",
-					order = 20,
-				},
-				split_meat = {
-					name = L[DB.cooking.meat.name],
-					desc = L[DB.cooking.meat.desc],
-					type = "toggle",
-					order = 30,
-				},
-				split_fish = {
-					name = L[DB.cooking.fish.name],
-					desc = L[DB.cooking.fish.desc],
-					type = "toggle",
-					order = 40,
-				},
-				split_reagents = {
-					name = L[DB.cooking.reagents.name],
-					desc = L[DB.cooking.reagents.desc],
-					type = "toggle",
-					order = 50,
-				},
-			}
-		},
-
 		gather = {
 			name = L["Gathering"],
 			type = "header",
 			order = 20,
 		},
-		move_herbs = {
-			name = L[DB.herbs.name],
-			desc = L[DB.herbs.desc],
+	}
+
+	for key, value in pairs(DB) do
+		options[key] = {
+			name = L[value.name],
+			desc = L[value.desc],
 			type = "toggle",
-			order = 21,
-		},
-		move_leather = {
-			name = L[DB.leather.name],
-			desc = L[DB.leather.desc],
-			type = "toggle",
-			order = 22,
-		},
-		move_ore_stone = {
-			name = L[DB.ore_stone.name],
-			desc = L[DB.ore_stone.desc],
-			type = "toggle",
-			order = 23,
-		},
-		move_cloth = {
-			name = L[DB.cloth.name],
-			desc = L[DB.cloth.desc],
-			type = "toggle",
-			order = 24,
-		},
-	}, AdiBags:GetOptionHandler(self, true, function() return self:Update() end)
+			order = value.order,
+		}
+		-- Handle categories with sections
+		if not value.items then
+			local args = {}
+			for k, v in pairs(value) do
+				-- Skip the text only values, we only want the child tables
+				if (type(v) == 'table') then
+					args[k] = {
+						name = L[v.name],
+						desc = L[v.desc],
+						type = "toggle",
+						order = v.order,
+					}
+				end
+			end
+			options[key .. "_splits"] = {
+				name = L["Split " .. value.category],
+				desc = "", -- doesn't seem to get used anyway
+				type = "group",
+				inline = true,
+				order = 18,
+				disabled = function() return not self.db.profile[key] end,
+				args = args
+			}
+		end
+	end
+
+	return options, AdiBags:GetOptionHandler(self, true, function() return self:Update() end)
 end
